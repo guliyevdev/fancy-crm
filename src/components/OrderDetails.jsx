c454532f2a79c5e8c285e0d2556d0752345896b4
AAllahverdi2 <agamaliyevallahverdi2@gmail.com>
2025-08-26 21:42:57 +0400

    bug fixed

--- src/components/OrderDetails.jsx
+++ src/components/OrderDetails.jsx
@@ -465,7 +465,7 @@ const OrderDetails = () => {
           <SiTaketwointeractivesoftware size={16} />
           {downloading ? "Processing..." : "Take Order"}
         </button>
-      </div>
+    </div>
 
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
