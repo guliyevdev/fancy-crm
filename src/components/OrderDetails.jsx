6cbe9026f613f196a2d247981496428e09edf92c
AAllahverdi2 <agamaliyevallahverdi2@gmail.com>
2025-10-03 02:04:57 +0400

    bug fix

--- src/components/OrderDetails.jsx
+++ src/components/OrderDetails.jsx
@@ -454,6 +454,7 @@ const OrderDetails = () => {
             </button>
           </>
         )}
+        
         <button
           onClick={() => setShowPaymentModal(true)}
           className="flex items-center justify-center gap-2 px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700 disabled:opacity-50 w-auto"
